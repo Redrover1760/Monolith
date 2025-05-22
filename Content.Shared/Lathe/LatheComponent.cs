916a8bbbdcf33b739c69ec29a70024ddbb9fed5b
Redrover1760 <alexander.wittkoff@gmail.com>
2025-05-22 00:17:08 -0400

    Aaaa2

--- Content.Shared/Lathe/LatheComponent.cs
+++ Content.Shared/Lathe/LatheComponent.cs
@@ -122,7 +122,7 @@ namespace Content.Shared.Lathe
         /// If not null, finite and non-negative, modifies values on spawned items
         /// </summary>
         [DataField]
-        public float? ProductValueModifier = 0.9f;
+        public float? ProductValueModifier = 1.2f;
         // End Frontier
         #endregion
     }
