8d7b4633acf1a2496eeab7758264aff71f6ce7c9
NazrinNya <macrohard@macrohard.localhost>
2025-12-18 00:06:28 +0300

    dfdfd

--- Content.Server/_Obelisk/Species/Systems/HydrakinSystem.cs
+++ Content.Server/_Obelisk/Species/Systems/HydrakinSystem.cs
@@ -4,15 +4,21 @@
 
 using Content.Server.Actions;
 using Content.Server.Popups;
+using Content.Server.Spawners.Components;
 using Content.Server.Species.Systems.Components;
 using Content.Server.Temperature.Components;
+using Content.Server.Temperature.Systems;
+using Content.Shared._Mono.Species.Systems;
 using Content.Shared.Actions;
 using Content.Shared.Actions.Events;
 using Content.Shared.Audio;
+using Content.Shared.DoAfter;
 using Content.Shared.IdentityManagement;
 using Content.Shared.Mobs;
 using Content.Shared.Mobs.Components;
+using Content.Shared.Temperature.Systems;
 using Robust.Shared.Audio.Systems;
+using Robust.Shared.Serialization;
 
 namespace Content.Server.Species.Systems;
 
@@ -21,12 +27,15 @@ public sealed class HydrakinSystem : EntitySystem
     [Dependency] private readonly SharedActionsSystem _actionsSystem = default!;
     [Dependency] private readonly PopupSystem _popupSystem = default!;
     [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly TemperatureSystem _temp = default!;
+    [Dependency] private readonly SharedDoAfterSystem _doAfter = default!;
 
     public override void Initialize()
     {
         base.Initialize();
         SubscribeLocalEvent<HydrakinComponent, ComponentInit>(OnInit);
         SubscribeLocalEvent<HydrakinComponent, HydrakinCoolOffActionEvent>(OnCoolOff);
+        SubscribeLocalEvent<HydrakinComponent, CoolOffDoAfterEvent>(OnCoolOffDoAfter);
     }
 
     public override void Update(float frameTime)
@@ -36,6 +45,12 @@ public sealed class HydrakinSystem : EntitySystem
         var query = EntityQueryEnumerator<HydrakinComponent, TemperatureComponent>();
         while (query.MoveNext(out var uid, out var comp, out var temperature))
         {
+            if (comp.CurrentTemperatureCooldown > TimeSpan.Zero)
+            {
+                comp.CurrentTemperatureCooldown -= TimeSpan.FromSeconds(frameTime);
+                return;
+            }
+
             if (!comp.HeatBuildupEnabled)
                 continue;
 
@@ -47,7 +62,8 @@ public sealed class HydrakinSystem : EntitySystem
                 temperature.CurrentTemperature > comp.MaxTemperature)
                 return;
 
-            temperature.CurrentTemperature += comp.Buildup * frameTime;
+            _temp.ChangeHeat(uid,comp.Buildup * comp.TemperatureProcessingCooldown * _temp.GetHeatCapacity(uid), true);
+            comp.CurrentTemperatureCooldown = TimeSpan.FromSeconds(comp.TemperatureProcessingCooldown);
         }
     }
 
@@ -61,10 +77,25 @@ public sealed class HydrakinSystem : EntitySystem
 
     private void OnCoolOff(EntityUid uid, HydrakinComponent component, HydrakinCoolOffActionEvent args)
     {
-        _popupSystem.PopupEntity(Loc.GetString("hydrakin-cool-off-emote", ("name", Identity.Entity(uid, EntityManager))), uid);
-        // TODO: sound effect
-        // TODO: do after
-        // TODO: cool down
+        var doafter = new DoAfterArgs(EntityManager, uid, TimeSpan.FromSeconds(3), new CoolOffDoAfterEvent(), uid);
+
+        if (!_doAfter.TryStartDoAfter(doafter))
+            return;
+
+        args.Handled = true;
+    }
+
+    private void OnCoolOffDoAfter(Entity<HydrakinComponent> ent, ref CoolOffDoAfterEvent args)
+    {
+        _popupSystem.PopupEntity(Loc.GetString("hydrakin-cool-off-emote", ("name", Identity.Entity(ent, EntityManager))), ent);
+        _audio.PlayEntity(ent.Comp.CoolOffSound, ent, ent);
+
+        if (!TryComp<TemperatureComponent>(ent, out var temp))
+            return;
+
+        _temp.ChangeHeat(ent,
+            (temp.CurrentTemperature * ent.Comp.CoolOffCoefficient - temp.CurrentTemperature) * _temp.GetHeatCapacity(ent),
+            true);
 
         args.Handled = true;
     }
